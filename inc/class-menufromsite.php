32d5cb773b4a4dd15f8006c6752ae6643bd49b92
Kate Hall <info@thirdboxcar.com>
2015-06-18 14:58:05 -0700

    Revisions for multiple menu inclusions
    Updates to documentation as well!

--- inc/class-menufromsite.php
+++ inc/class-menufromsite.php
@@ -7,7 +7,7 @@ class MasterSharedMenu {
 
 	public function __construct() {
 		$this->plugin_name = 'multisitesharedmenu';
-		$this->version = '1.1';
+		$this->version = '1.2';
 
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
@@ -15,33 +15,29 @@ class MasterSharedMenu {
 
 	private function define_admin_hooks() {
 		$this->add_mfs_menu();
-
 	}
 
-	private function define_public_hooks() {		
+	private function define_public_hooks() {
 		add_filter( 'pre_wp_nav_menu' , array( $this, 'menuswitch_check'), 10, 2 );
 		add_filter( 'wp_nav_menu_items', array( $this, 'check_restore_blog_menu' ), 10, 2 );
-
 	}
 
 	// Checks if options set to switch menu. If so, switch to the appropriate site and change the menu to load the desired menu....
 	public function menuswitch_check( $a, $menu_object ) {
-		
 		if( !( $mfsSettings = $this->validate_mfs_set() )) {
 			return false;
 		}
 		
 		$navigation_affected = $mfsSettings['destinationMenuLocation'];
-		if (isset( $menu_object )) {
-			
-			if( $mfsSettings['destinationMenuLocation'] == $menu_object->theme_location ) {
-				
-				$switchSite = get_blog_details($mfsSettings['sourceSiteID']);
-				switch_to_blog($switchSite->blog_id);
-				
-			}		
+		// nav menus now stored as an array, loop through each item
+		foreach ( $navigation_affected as $menu) {
+			if (isset( $menu_object )) {
+				if( $menu == $menu_object->theme_location ) {
+					$switchSite = get_blog_details($mfsSettings['sourceSiteID']);
+					switch_to_blog($switchSite->blog_id);
+				}
+			}
 		}
-		
 		return;
 	}
 	
@@ -58,12 +54,12 @@ class MasterSharedMenu {
 		$destinationMenuLocation = get_option( 'mfs_override_menu_location' );
 
 		if(( 	!strlen( $sourceSiteID ) ||
-				!strlen( $destinationMenuLocation ) ||
+				empty( $destinationMenuLocation ) ||
 				!is_numeric ($sourceSiteID ))) {
 					
 			return false;
 		}
-		
+
 		return array( 'sourceSiteID' => $sourceSiteID, 'destinationMenuLocation' => $destinationMenuLocation );
 	}
 
