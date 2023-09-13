6e935328368346adbb29b6c8ad99eb3fb06ff713
Ben Greeley <ben.greeley@automattic.com>
2023-09-13 10:14:00 -0400

    Rework plugin structure to be more modern and flexible, introducing Composer and class autoloading.

--- multisitesharedmenu.php
+++ multisitesharedmenu.php
@@ -1,13 +1,16 @@
 <?php
-/*
- * Plugin Name:       Multisite Master Shared Menu
+/**
+ * Plugin Name:       Multisite Shared Menu
  * Plugin URI:        http://www.bengreeley.com/menufromsite
- * Description:       Allows users in a WordPress multisite network pull in a menu from another site in order to achieve a universal navigation or shared navigation without needing to manually recreate menus. Site is required to be installed on WordPress Multisite environment.
+ * Description:       Allows users in a WordPress multisite network pull in a menu
+ * from another site in order to achieve a universal navigation or shared navigation
+ * without needing to manually recreate menus. Site is required to be installed on
+ * WordPress Multisite environment.
  * Version:           1.2
  * Author:            Ben Greeley
  * Author URI:        http://www.bengreeley.com
- */
- 
+ **/
+
  /*  
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -23,10 +26,15 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
-}
+require_once plugin_dir_path( __FILE__) . 'inc/class-menufromsite.php';
+
+// Useful global constants.
+define( 'MULTISITE_SHARED_MENU_PLUGIN_VERSION', '2.0' );
+define( 'MULTISITE_SHARED_MENU_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'MULTISITE_SHARED_MENU_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'MULTISITE_SHARED_MENU_PLUGIN_INC', MULTISITE_SHARED_MENU_PLUGIN_PATH . 'includes/' );
+define( 'MULTISITE_SHARED_MENU_PLUGIN_DIST_URL', MULTISITE_SHARED_MENU_PLUGIN_URL . 'dist/' );
+define( 'MULTISITE_SHARED_MENU_PLUGIN_DIST_PATH', MULTISITE_SHARED_MENU_PLUGIN_PATH . 'dist/' );
 
 // Activation code for plugin to create necessary fields, etc.
 function activate_menufromsite() {
@@ -44,10 +52,7 @@ function deactivate_menufromsite() {
 
 register_deactivation_hook( __FILE__, 'deactivate_menufromsite' );
 
-require plugin_dir_path( __FILE__) . 'inc/class-menufromsite.php';
-
-if( is_multisite() ) {
+if ( function_exists( 'is_multisite' ) && is_multisite() ) {
 	// Only run this menu if in multisite...
 	new MasterSharedMenu();
-	
 }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wordpress/multisitesharedmenu',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '50845a2d1eda56dd78ceb062ed6019904047262d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'haydenpierce/class-finder' => array(
+            'pretty_version' => '0.4.4',
+            'version' => '0.4.4.0',
+            'reference' => '94c602870ddf8d4fa2d67fb9bae637d88f9bd76e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../haydenpierce/class-finder',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.7.2',
+            'version' => '3.7.2.0',
+            'reference' => 'ed8e00df0a83aa96acf703f8c2979ff33341f879',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'wordpress/multisitesharedmenu' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '50845a2d1eda56dd78ceb062ed6019904047262d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
